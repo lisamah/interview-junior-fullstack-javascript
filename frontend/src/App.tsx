f7688c0bb85c0380430189a1ec95a980ddbdd6f7
l.hoell <lisa_hoell97@web.de>
2023-11-23 13:28:06 +0100

    Add frontend form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useForm, SubmitHandler } from 'react-hook-form'
 import './App.css'
 
+type FieldValues = {
+  city: string
+};
+
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    register, 
+    handleSubmit, 
+    formState: {errors},
+  } = useForm<FieldValues>();
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => console.log(data)
+  // const onSubmit = (data: FieldValues) => {console.log('gesendet', data)}
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>City:</label>
+        <input type='text' id='city' {...register('city', {required: true})} />
+        <input type='submit' value='senden' />
+
+        {errors.city && <p className='form-error'>Eingabe erforderlich</p>}
+      </form>
     </>
   )
 }
 
-export default App
+export default App;
